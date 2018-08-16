f30a933ecf5231997a33a028fa2483d228854464
hzhlu <hzhlu@sina.com>
2018-08-16 18:30:32 +0800

    snapshot 显示主界面

--- src/com/macrolab/myAcct/Main.java
+++ src/com/macrolab/myAcct/Main.java
@@ -6,10 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        File mainFXML = new File("D:\\mySCM\\gitRepo\\myAcct\\myAcct.fxml");
+//        Parent root = FXMLLoader.load(getClass().getResource(mainFXML.getAbsolutePath()));
         Parent root = FXMLLoader.load(getClass().getResource("myAcct.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
